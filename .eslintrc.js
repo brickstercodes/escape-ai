fa15c24090302d531076204ebadea8fbbb757a1a
Anugrah Shetty <anuadz2358@gmail.com>
2025-03-03 08:30:00 +0530

    lint+minor package json issue

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
   rules: {
     // Disable rules that might cause issues with newer TypeScript versions
     "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": "off",
 
     // Disable the empty function rule
     "@typescript-eslint/no-empty-function": "off",
